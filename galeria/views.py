eb11f0e8750d3a143f3ab755f236becaf8f3902c
ArthurOReis <arthuroreis2005@gmail.com>
2023-02-27 14:51:04 -0300

    Criando página 'imagem'

--- galeria/views.py
+++ galeria/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'galeria/index.html') # Tirando HttpResponse e, usando render, vai fazer com que a função procure um arquivo HTML através de um endereço
+
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
